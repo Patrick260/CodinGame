5e90846e53885a05262823247f4fa4055842f69f
Patrick260 <Patrick260@protonmail.com>
2022-03-10 18:50:01 +0100

    Add solution for Classic-Puzzle / Easy /  Onboarding

---
+++ Classic-Puzzles/Easy/Onboarding.java
@@ -0,0 +1,27 @@
+/*
+    Copyright (c) 2022 Patrick260
+    Distributed under the terms of the MIT License.
+*/
+
+import java.util.Scanner;
+
+final class Player {
+
+    public static void main(final String args[]) {
+
+        final Scanner input = new Scanner(System.in);
+
+        while (true) {
+
+            String enemy1 = input.next();
+            int dist1 = input.nextInt();
+            String enemy2 = input.next();
+            int dist2 = input.nextInt();
+
+            System.out.println(dist1 < dist2 ? enemy1 : enemy2);
+
+        }
+
+    }
+  
+}
