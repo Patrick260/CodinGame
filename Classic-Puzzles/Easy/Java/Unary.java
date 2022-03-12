a1495216549403d30c39572725cde4161e9f3b7a
Patrick260 <Patrick260@protonmail.com>
2022-03-12 18:07:27 +0100

    Refactor Unary.java

--- Classic-Puzzles/Easy/Java/Unary.java
+++ Classic-Puzzles/Easy/Java/Unary.java
@@ -10,39 +10,40 @@ final class Solution {
     public static void main(final String[] args) {
 
         final Scanner input = new Scanner(System.in);
-        final String MESSAGE = input.nextLine();
 
-        final StringBuilder encoded_string = new StringBuilder();
+        final String message = input.nextLine();
+
+        final StringBuilder encodedString = new StringBuilder();
 
         Character previous = '2';
 
-        for(final Character c : MESSAGE.toCharArray()) {
+        for(final Character c : message.toCharArray()) {
 
-            final StringBuilder binary_string = new StringBuilder(Integer.toBinaryString(c));
+            final StringBuilder binaryString = new StringBuilder(Integer.toBinaryString(c));
 
-            while (binary_string.length() < 7) {
+            while (binaryString.length() < 7) {
 
-                binary_string.insert(0, "0");
+                binaryString.insert(0, "0");
 
             }
 
-            for (final Character cc : binary_string.toString().toCharArray()) {
+            for (final Character cc : binaryString.toString().toCharArray()) {
 
                 if (cc != previous) {
 
                     if (cc == '0') {
 
-                        encoded_string.append(" 00 ");
+                        encodedString.append(" 00 ");
 
                     } else {
 
-                        encoded_string.append(" 0 ");
+                        encodedString.append(" 0 ");
 
                     }
 
                 }
 
-                encoded_string.append(0);
+                encodedString.append(0);
 
                 previous = cc;
 
@@ -50,7 +51,7 @@ final class Solution {
 
         }
 
-        System.out.println(encoded_string.toString().replaceFirst(" ", ""));
+        System.out.println(encodedString.toString().replaceFirst(" ", ""));
 
     }
 
