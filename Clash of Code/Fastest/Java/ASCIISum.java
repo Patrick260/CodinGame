ad863ba94e992d5db1379edef74db034ee3b8f07
Patrick260 <Patrick260@protonmail.com>
2022-03-15 15:26:15 +0100

    Add solution for Clash-of-Code / Fastest / ASCII Sum

---
+++ Clash of Code/Fastest/Java/ASCIISum.java
@@ -0,0 +1,28 @@
+/*
+    Copyright (c) 2022 Patrick260
+    Distributed under the terms of the MIT License.
+*/
+
+import java.util.Scanner;
+
+final class Solution {
+
+    public static void main(final String[] args) {
+
+        final Scanner input = new Scanner(System.in);
+
+        final String s = input.nextLine();
+
+        int sum = 0;
+
+        for (final Character c : s.toCharArray()) {
+
+            sum += c;
+
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
