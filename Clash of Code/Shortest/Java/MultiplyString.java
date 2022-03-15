f336e9c9ac17edb11824fc3042580ab464d883a3
Patrick260 <Patrick260@protonmail.com>
2022-03-15 16:28:48 +0100

    Add solution for Clash-of-Code / Fastest / MultiplyString.java

---
+++ Clash of Code/Shortest/Java/MultiplyString.java
@@ -0,0 +1,25 @@
+/*
+    Copyright (c) 2022 Patrick260
+    Distributed under the terms of the MIT License.
+*/
+
+import java.util.Scanner;
+
+final class Solution {
+
+    public static void main(final String[] args) {
+
+        final String s = new Scanner(System.in).next();
+
+        int p = Integer.parseInt("" + s.charAt(0));
+
+        for (int i = 1; i < s.length(); i++) {
+
+            p *= Integer.parseInt("" + s.charAt(i));
+
+        }
+
+        System.out.println(p);
+    }
+
+}
