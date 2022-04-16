46dfa3c8084d05925571e02fc3da2ad75b014457
Patrick260 <Patrick260@protonmail.com>
2022-04-16 17:44:30 +0200

    Add Java solution for Clash-of-Code / Reverse / List odd numbers

---
+++ Clash of Code/Reverse/Java/ListOddNumbers.java
@@ -0,0 +1,24 @@
+/*
+    Copyright (c) 2022 Patrick260
+    Distributed under the terms of the MIT License.
+*/
+
+import java.util.Scanner;
+
+final class Solution {
+
+    public static void main(final String[] args) {
+
+        final Scanner input = new Scanner(System.in);
+
+        final int n = input.nextInt();
+
+        for (int i = 1; i <= n; i += 2) {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}
