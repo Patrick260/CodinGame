81d1daec86aa976de206a986e02bb246f934e992
Patrick260 <Patrick260@protonmail.com>
2022-04-16 19:42:57 +0200

    Add Java solution for Clash-of-Code / Fastest / Print word n times

---
+++ Clash of Code/Fastest/Java/PrintWordNTimes.java
@@ -0,0 +1,25 @@
+/*
+    Copyright (c) 2022 Patrick260
+    Distributed under the terms of the MIT License.
+*/
+
+import java.util.Scanner;
+
+final class Solution {
+
+    public static void main(final String[] args) {
+
+        final Scanner input = new Scanner(System.in);
+
+        final int n = input.nextInt();
+        final String word = input.next();
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println(word);
+
+        }
+
+    }
+
+}
